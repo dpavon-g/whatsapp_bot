8fa32c90974d1e44089a6ba774dd5a560fc29a34
flakkpei <pavongomezdaniel@gmail.com>
2021-11-05 10:49:26 +0100

    Final bot

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on('qr', qr => {
 	qrcode.generate(qr, {small: true});
 });
 client.on('ready', () => {
-	console.log('El cliente esta listo');
+	console.log('Client ready');
 })
 client.on('message', message => {
 	console.log(message.body);
@@ -29,7 +29,7 @@ client.on('message', message => {
 client.on('message', message => {
 	if(message.body === '!ayuda') {
 		message.reply('Hola! Soy Jumanji tu bot de confianza y cada dia el de mas gente. Aqui te dejo un listado de comandos que puedes ejecutar:\
-        \n\n1. !ayuda\n2. !condiciones\n3. !saludo\n4. !sticker\n5. Por el culo te la hinco\n6. !final\n7. !version\n8. !extra');
+        \n\n1. !ayuda\n2. !condiciones\n3. !saludo\n4. !sticker\n5. !version');
 	}
     else if(message.body === '!version')
         message.reply('Version actual: 8.0')
@@ -39,12 +39,6 @@ client.on('message', message => {
     }
     else if(message.body === '!saludo')
         message.reply('Hola mundo!!')
-    else if(message.body === '!final')
-        message.reply('Probablente a principios de Noviembre dejare de funcionar. \nAsi que disfrutad de mi mientras dure :D')
-    else if(message.body === '!extra')
-        message.reply('Hermano que hambre, decidle a Pablo que me ponga un campero')
-    else if(message.body === 'Por el culo te la hinco')
-        message.reply('JAJAJAJA')
 
 });
 client.on('message', async msg => {
